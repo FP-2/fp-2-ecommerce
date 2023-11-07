da982b54359dc29f4c2f21b00535c9b481f5a3c8
zaki fauzan <zakifauzanrabb@gmail.com>
2023-11-07 11:23:22 +0800

    add redux api version

--- src/redux/productSlice.js
+++ src/redux/productSlice.js
@@ -1,15 +1,53 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { productsData } from "../api/Api";
 
 const initialState = {
   productData: [],
   userInfo: null,
+  loading: false,
+  error: null,
 };
 
 export const productSlice = createSlice({
   name: "product",
   initialState,
-  reducers: {},
+  reducers: {
+    setProductData: (state, action) => {
+      state.productData = action.payload;
+    },
+    setUserInfo: (state, action) => {
+      state.userInfo = action.payload;
+    },
+    fetchProductsStart: (state) => {
+      state.loading = true;
+      state.error = null;
+    },
+    fetchProductsSuccess: (state, action) => {
+      state.products = action.payload;
+      state.loading = false;
+    },
+    fetchProductsFailure: (state, action) => {
+      state.loading = false;
+      state.error = action.payload;
+    },
+  },
 });
 
-export const {} = productSlice.actions;
+export const {
+  setProductData, 
+  setUserInfo, 
+  fetchProductsStart,
+  fetchProductsSuccess,
+  fetchProductsFailure,
+} = productSlice.actions;
 export default productSlice.reducer;
+
+export const fetchProducts = () => async (dispatch) => {
+  try {
+    dispatch(fetchProductsStart());
+    const response = await productsData();
+    dispatch(fetchProductsSuccess(response.data));
+  } catch (error) {
+    dispatch(fetchProductsFailure(error.message));
+  }
+};
